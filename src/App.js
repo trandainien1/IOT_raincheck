4488ca540a79ccdbfad7ad33150f2e46c6591268
trandainien1 <trandainien1@gmail.com>
2023-11-15 22:05:38 +0700

    data sync between web and firestore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./firebaseConfig"; // Add this line prevent firebase not loading error
-import { getFirestore, addDoc, collection } from "firebase/firestore";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  onSnapshot,
+} from "firebase/firestore";
 import { getDocs } from "firebase/firestore";
 
 function App() {
@@ -18,41 +23,37 @@ function App() {
     alert("Document written to Database");
   };
 
-  const fetchDataFromFirestore = async () => {
-    const querySnapshot = await getDocs(collection(db, "EspData"));
-    const temporaryArr = [];
-    querySnapshot.forEach((doc) => {
-      temporaryArr.push(doc.data());
+  const fetchDataFromFirestore = () => {
+    const query = collection(db, "EspData");
+
+    const unsubscribe = onSnapshot(query, (querySnapshot) => {
+      const temporaryArr = [];
+      querySnapshot.forEach((doc) => {
+        temporaryArr.push(doc.data());
+      });
+      console.log(temporaryArr);
+      setStoredValues(temporaryArr);
     });
-    console.log(temporaryArr);
-    setStoredValues(temporaryArr);
+
+    return unsubscribe;
   };
+
+  useEffect(() => {
+    const unsubscribe = fetchDataFromFirestore();
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Save Data to Firebase Firestore</h1>
-      <input
-        type="text"
-        value={inputValue1}
-        onChange={(e) => setInputValue1(e.target.value)}
-      />
-      <input
-        type="text"
-        value={inputValue2}
-        onChange={(e) => setInputValue2(e.target.value)}
-      />
-      <button onClick={saveDataToFirestore}>Save to Firestore</button>
-
-      <button onClick={fetchDataFromFirestore}>Fetch from Firestore</button>
-
-      <h2>Stored Values</h2>
-      <ul>
-        {storedValues.map((value, index) => (
-          <li key={index}>
-            Humidity: {value.Humidity}, Temperature: {value.Temperature}
-          </li>
-        ))}
-      </ul>
+      <h1>Data from Firestore</h1>
+      {storedValues.map((value) => (
+        <div>
+          <div>Temperature: {value.Temperature}</div>
+          <div>Humidity: {value.Humidity}</div>
+          <div>Light: {value.Light}</div>
+          <div>Rain: {value.Rain}</div>
+        </div>
+      ))}
     </div>
   );
 }
